604db1fb1955b06d9e27ba8c4a6e461d497b80b9
Aanchal Upadhayay <uaanchal551999@gmail.com>
2021-05-20 12:17:04 +0530

    update and Delete call

--- LearnSpringMvc/src/main/java/mfsi/learnmvc/rest/EmployeeController.java
+++ LearnSpringMvc/src/main/java/mfsi/learnmvc/rest/EmployeeController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 import mfsi.learnmvc.dto.EmployeeDto;
 import mfsi.learnmvc.service.EmployeeService;
@@ -15,7 +18,7 @@ import mfsi.learnmvc.service.EmployeeService;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeService service;
 
@@ -24,22 +27,30 @@ public class EmployeeController {
 		ResponseEntity<?> response = new ResponseEntity<>(service.getAll(), HttpStatus.OK);
 		return response;
 	}
-	
+
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public ResponseEntity<?> save(@RequestBody EmployeeDto dto){
+	public ResponseEntity<?> save(@RequestBody EmployeeDto dto) {
 		ResponseEntity<?> response = new ResponseEntity<>(service.save(dto), HttpStatus.OK);
 		return response;
 	}
-	
+
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
-	public void delete(@PathVariable Integer id){
+	public void delete(@PathVariable Integer id) {
 		service.delete(id);
 	}
-	
-	@RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
-	public void update(@RequestBody EmployeeDto dto,@PathVariable Integer id ){
-		service.update(dto, id);
-		
+
+	@RequestMapping(value = "/update", method = RequestMethod.PUT)
+	public ResponseEntity<?> update(@RequestBody EmployeeDto dto) {
+		ResponseEntity<?> response = new ResponseEntity<>(service.update(dto), HttpStatus.OK);
+		return response;
 	}
 
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	public ResponseEntity<?> search(@RequestParam(value ="id") int id) {
+		ResponseEntity<?> response = new ResponseEntity<>(service.search(id), HttpStatus.OK);
+		return response;
+	}
+
+
+	
 }
--- LearnSpringMvc/src/main/java/mfsi/learnmvc/service/EmployeeService.java
+++ LearnSpringMvc/src/main/java/mfsi/learnmvc/service/EmployeeService.java
@@ -3,6 +3,7 @@ package mfsi.learnmvc.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -26,14 +27,22 @@ public class EmployeeService {
 		employees.removeIf(t -> t.getId().equals(id));
 	}
 
-	public void update(EmployeeDto dto, Integer id) {
-		for (int i = 0; i < employees.size(); i++) {
+	public EmployeeDto update(EmployeeDto dto) {
+		/*for (int i = 0; i < employees.size(); i++) {
 			EmployeeDto d = employees.get(i);
-			if (d.getId().equals(id)) {
+			if (d.getId().equals(dto.getId())) {
 				employees.set(i, dto);
 				return;
 			}
-		}
+		}*/
+		int index = employees.indexOf(dto);
+		employees.set(index, dto);
+		return dto;
 	}
 
+	public EmployeeDto search(Integer id) {
+		
+		return employees.stream().filter(t -> t.getId().equals(id)).findAny().orElse(null);
+		
+	}
 }
